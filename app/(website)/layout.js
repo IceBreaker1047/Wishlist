3518e958de71872a7c18de784c73d2ac9acaac3a
Ansh Rajpal <anshrajpal7@gmail.com>
2025-09-13 14:08:33 +0530

    added analytics

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -1,11 +1,13 @@
 import Header from "@/components/ui/header";
 import React, { Children } from "react";
+import { Analytics } from "@vercel/analytics/next"
 
-const layout = ({children}) => {
+const layout = ({ children }) => {
     return (
         <div>
             <Header />
             {children}
+            <Analytics />
         </div>
     )
 }
